c809921bcc10c470be8b32727b072301a6998200
LuK1337 <priv.luk@gmail.com>
2020-05-16 20:28:31 +0200

    sdm660-common: Build 64-bit variants of android.hardware.camera.{device,provider}
    * Fixes: ims_media_jni: Error loading library lib-imsvt.so: dlopen failed: library "android.hardware.camera.provider@2.4.so" not found
    
    Change-Id: I938dd7135a53e9e0951c31e7f56082c0564cf924

--- sdm660.mk
+++ sdm660.mk
@@ -167,12 +167,15 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.device@3.5 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    camera.device@3.2-impl \
+    android.hardware.camera.provider@2.5 \
     libstdc++.vendor \
     libxml2 \
-    Snap
+    Snap \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0_vendor
 
 # Component overrides
 PRODUCT_COPY_FILES += \
