85c0202aa57fe64870de5f5d161aca42c9b43d71
sjllls <2012ji123@gmail.com>
2020-09-27 09:03:59 +0800

    sdm660-common: RIL fixed

--- sdm660.mk
+++ sdm660.mk
@@ -302,7 +302,11 @@ PRODUCT_PACKAGES += \
 
 # Radio
 PRODUCT_PACKAGES += \
+    android.hardware.radio@1.4 \
     android.hardware.radio@1.2 \
+    android.hardware.radio.config@1.0 \
+    android.hardware.secure_element@1.0 \
+    rild \
     librmnetctl \
     libprotobuf-cpp-full \
     libxml2
